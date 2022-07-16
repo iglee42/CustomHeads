ba44c90ad3f95df38fa2c9d9d742f4ff65743132
iglée42 <igleev2@gmail.com>
2022-07-16 19:46:02 +0200

    Base

---
+++ CustomHead.java
@@ -0,0 +1,61 @@
+package customheads;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.SkullMeta;
+
+public abstract class CustomHead implements Listener{
+	
+	private String name,headName;
+	private List<String> lore = new ArrayList<>();
+
+	/**
+	 * @param name
+	 * @param headName
+	 */
+	public CustomHead(String name, String headName) {
+		this.name = name;
+		this.headName = headName;
+		HeadsManager.customHead.put(name, this);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getHeadName() {
+		return headName;
+	}
+	
+	public void activate(Player p) {
+		HeadsManager.activatedHead.put(p, this);
+		p.getInventory().setItemInMainHand(null);
+	}
+	public void desactivate(Player p) {
+		HeadsManager.activatedHead.remove(p);
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (!(obj instanceof CustomHead)) return false;
+		boolean flag1 = ((CustomHead)obj).getName() == this.getName();
+		return flag1;
+	}
+	
+	@SuppressWarnings("deprecation")
+	public ItemStack build() {
+		ItemStack itemStack = new ItemStack(Material.SKULL_ITEM, 1, (byte) 3);
+		SkullMeta itemMeta = (SkullMeta) itemStack.getItemMeta();
+		itemMeta.setDisplayName(name.replaceAll("&", "§"));
+		itemMeta.setOwner(headName);
+		if (lore != null) itemMeta.setLore(lore);
+		itemStack.setItemMeta(itemMeta);
+		return itemStack;
+	}
+
+}
---
+++ HeadsManager.java
@@ -0,0 +1,46 @@
+package customheads;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.plugin.Plugin;
+
+public class HeadsManager implements Listener {
+	
+	public static Map<String,CustomHead> customHead = new HashMap<>();
+	public static Map<Player,CustomHead> activatedHead = new HashMap<>();
+	
+	public HeadsManager(Plugin plugin) {
+		Bukkit.getPluginManager().registerEvents(this, plugin);
+	}
+
+	public static void addHead(CustomHead head,Plugin plugin) {
+		Bukkit.getPluginManager().registerEvents(head, plugin);
+	}
+	
+	@EventHandler
+	public void onInteract(PlayerInteractEvent event) {
+		if (event.getAction() == null && event.getItem() == null) return;
+		if (event.getAction() == Action.LEFT_CLICK_AIR || event.getAction() == Action.LEFT_CLICK_BLOCK) return;
+		if (!event.getItem().hasItemMeta()) return;
+		if (!event.getItem().getItemMeta().hasDisplayName()) return;
+		if (event.getItem().getType() == Material.SKULL_ITEM) {
+			CustomHead h = null;
+			for (CustomHead head : customHead.values()) {
+				String hn = head.getName().replaceAll("&", "§");
+				if (event.getItem().getItemMeta().getDisplayName().equals(hn)) {
+					h = head;
+				}
+			}
+			if (h == null) return;
+			h.activate(event.getPlayer());
+		}
+	}
+}
